b164de4d4a68682dd71e99a5fefe83374ea47370
akashfd <ahsanshamim193@gmail.com>
2024-04-22 17:13:58 +0600

    datatype practice

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+// numbr
+let x = 5;
+let y = 6 ;
+console.log(typeof x)
+
+// number
+let name= "akash"
+let phone= 4411
+console.log (typeof phone)
+
+// string
+let address = ""
+let brand = "bmw"
+console.log ( typeof brand)
+
+
+
